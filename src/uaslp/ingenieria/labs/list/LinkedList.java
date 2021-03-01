ac6c5b68e86f09715efddd3cfeb3995e51e22901
Luis Naranjo <luise_nc@outlook.com>
2021-03-01 08:22:31 -0600

    Separar clase Node

--- src/uaslp/ingenieria/labs/list/LinkedList.java
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -125,36 +125,3 @@ public class LinkedList {
     }
 }
 
-class Node{
-    private int data;
-    private Node next;
-    private Node previous;
-
-    Node(int data){
-        this.data = data;
-    }
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Node getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(Node previous) {
-        this.previous = previous;
-    }
-}
